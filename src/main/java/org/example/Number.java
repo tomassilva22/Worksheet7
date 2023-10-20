3c63333074d055ae29ac87c8c6c7c5734c4c2d29
tomas <tomasoliveira208@gmail.com>
2023-10-20 12:29:22 +0100

    Initial commit

---
+++ src/main/java/org/example/Number.java
@@ -0,0 +1,21 @@
+public class Number {
+
+    public boolean isPrime(int number) {
+
+    }
+
+
+    public boolean isEven(int number) {
+        if ((number % 2) == 0) {
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    public boolean isMultiple(int number, int divisor) {
+        
+    }
+}
